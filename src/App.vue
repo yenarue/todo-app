14b7fc6d6fdb300a6ef5493b759ea4556e41159d
yenarue <yenarue@gmail.com>
2020-05-13 20:48:31 +0900

    두번째 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,41 @@
 <template>
   <div id="app">
-    <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodo="addTodo"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
-    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+    <!-- <p>{{msg}}</p>
+    <main v-if="show">메인</main>-->
+    <todo-header></todo-header>
+    <!-- TodoInput, TodoList 컴포넌트 등록  -->
+    <!-- 1. 컴포넌트 폴더 밑에 vue 파일 생성
+    2. vue 파일에서 `vue` 자동 완성 기능으로 기본 코드 생성
+    3. 각 template 코드에 `<div>컴포넌트 이름</div>` 기입
+    4. 컴포넌트를 모두 App.vue 파일에 등록 후 표시-->
+    <todo-input></todo-input>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
+// import 컴포넌트 이름 from '컴포넌트 경로';
+// 스니펫 : vim
 import TodoHeader from "./components/TodoHeader.vue";
-import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
-import TodoFooter from "./components/TodoFooter.vue";
+import TodoInput from "@/components/TodoInput.vue";
 
 export default {
+  // 스니펫 : vda
   data() {
     return {
-      todoItems: []
+      msg: "hi",
+      show: false
     };
   },
-  methods: {
-    clearAll() {
-      localStorage.clear();
-      this.todoItems = [];
-    },
-    addTodo(todoItem) {
-      localStorage.setItem(todoItem, todoItem);
-      this.todoItems.push(todoItem);
-    },
-    removeTodo(todoItem, index) {
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1);
-    }
-  },
-  created() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        this.todoItems.push(localStorage.key(i));
-      }
-    }
-  },
   components: {
-    TodoHeader: TodoHeader,
-    TodoInput: TodoInput,
-    TodoList: TodoList,
-    TodoFooter: TodoFooter
+    // '컴포넌트 이름' : 컴포넌트 내용
+    "todo-header": TodoHeader,
+    "todo-input": TodoInput,
+    "todo-list": TodoList
   }
 };
 </script>
 
 <style>
-body {
-  text-align: center;
-  background-color: #f6f6f8;
-}
-input {
-  border-style: groove;
-  width: 200px;
-}
-button {
-  border-style: groove;
-}
-.shadow {
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
-}
 </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,78 +1,32 @@
 <template>
-  <div class="inputBox shadow">
-    <input
-      type="text"
-      v-model="newTodoItem"
-      placeholder="Type what you have to do"
-      v-on:keyup.enter="addTodo"
-    />
-    <span class="addContainer" v-on:click="addTodo">
-      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
-    </span>
-
-    <modal v-if="showModal" @close="showModal = false">
-      <h3 slot="header">경고</h3>
-      <span slot="footer" @click="showModal = false">
-        할 일을 입력하세요.
-        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
-      </span>
-    </modal>
+  <div>
+    <input type="text" v-model="inputText" v-on:keyup.enter="addItem" />
+    <button v-on:click="addItem">add</button>
   </div>
 </template>
 
 <script>
-import Modal from "./common/Modal.vue";
-
 export default {
   data() {
     return {
-      newTodoItem: "",
-      showModal: false
+      inputText: ""
     };
   },
   methods: {
-    addTodo() {
-      if (this.newTodoItem !== "") {
-        var value = this.newTodoItem && this.newTodoItem.trim();
-        this.$emit("addTodo", value);
-        this.clearInput();
-      } else {
-        this.showModal = !this.showModal;
-      }
+    addItem: function() {
+      localStorage.setItem(this.inputText, this.inputText);
+      this.clearInput();
+      // this.inputText;
+      // axios.post("todos", {
+      //   title: this.inputText
+      // });
     },
-    clearInput() {
-      this.newTodoItem = "";
+    clearInput: function() {
+      this.inputText = "";
     }
-  },
-  components: {
-    Modal: Modal
   }
 };
 </script>
 
 <style scoped>
-input:focus {
-  outline: none;
-}
-.inputBox {
-  background: white;
-  height: 50px;
-  line-height: 50px;
-  border-radius: 5px;
-}
-.inputBox input {
-  border-style: none;
-  font-size: 0.9rem;
-}
-.addContainer {
-  float: right;
-  background: linear-gradient(to right, #6478fb, #8763fb);
-  display: inline-block;
-  width: 3rem;
-  border-radius: 0 5px 5px 0;
-}
-.addBtn {
-  color: white;
-  vertical-align: middle;
-}
 </style>
